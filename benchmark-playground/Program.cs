106353b0ac750e1efe5716ac2b5c4f7218f4fab2
Albert Kliemke <akliemke@protonmail.com>
2023-05-05 22:51:25 -0300

    add first method

--- benchmark-playground/Program.cs
+++ benchmark-playground/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace benchmark_playground
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace benchmark_playground
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            BenchmarkRunner.Run<BenchAsync>();
+
             Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+    public class BenchAsync
+    {
+        [Benchmark]
+        public async Task<string> AsyncMethod()
+        {
+            await Task.Delay(1000);
+            return "Hello, World!";
         }
     }
 }
