caa018237fc5d9911aa004f6ca16c8b0794ab12f
Albert Kliemke <akliemke@protonmail.com>
2023-05-10 06:50:53 -0300

    adding long running tasks

---
+++ long-running-task/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace long_running_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public async Task LongRunningOp(CancellationToken cancellationToken)
+        {
+            await Task.Factory.StartNew(async () => {
+                await Task.Delay(2000);
+            }, 
+            cancellationToken,
+            TaskCreationOptions.LongRunning, TaskScheduler.Default
+           );
+
+        }
+    }
+}
