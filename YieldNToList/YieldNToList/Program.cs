a599d74edd17f4d33128c900533faa22eb0df2b9
Albert Kliemke <akliemke@protonmail.com>
2024-03-31 17:12:37 -0300

    understanding iterators with yield

---
+++ YieldNToList/YieldNToList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using BenchmarkDotNet;
+namespace YieldNToList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            //BenchmarkDotNet.Running.BenchmarkRunner.Run<YieldNToList>();
+            var yieldNToList = new YieldNToList();
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var x = yieldNToList.GetYieldSlow(10).ToList();
+            //var x = yieldNToList.GetYieldSlow(10);
+            Console.WriteLine(x.Sum());
+            Console.WriteLine(x.Sum());
+            stopWatch.Stop();
+            Console.WriteLine($"{stopWatch.ElapsedMilliseconds}ms");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ YieldNToList/YieldNToList/YieldNToList.cs
@@ -0,0 +1,58 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldNToList
+{
+    [MemoryDiagnoser]
+    public class YieldNToList
+    {
+        [Benchmark]
+        public int SumWithToList()
+        {
+            var sum = GetYield(1000).ToList().Sum();
+            return sum;
+        }
+
+        [Benchmark]
+        public int SumWithoutToList()
+        {
+            var sum = GetYield(1000).Sum();
+            return sum;
+        }
+
+        [Benchmark]
+        public int SumWithToListSlow()
+        {
+            var sum = GetYieldSlow(1000).ToList().Sum();
+            return sum;
+        }
+
+        [Benchmark]
+        public int SumWithoutToListSlow()
+        {
+            var sum = GetYieldSlow(1000).Sum();
+            return sum;
+        }
+
+        public IEnumerable<int> GetYieldSlow(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Thread.Sleep(1000);
+                yield return i;
+            }
+        }
+
+        public IEnumerable<int> GetYield(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
