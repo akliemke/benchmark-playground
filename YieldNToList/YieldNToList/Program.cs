e6ce9bc91784032b338cd52065e8a9b081d7c4bf
Albert Kliemke <akliemke@protonmail.com>
2024-03-31 17:15:24 -0300

    adding comments

--- YieldNToList/YieldNToList/Program.cs
+++ YieldNToList/YieldNToList/Program.cs
@@ -12,8 +12,9 @@ namespace YieldNToList
 
             var stopWatch = new Stopwatch();
             stopWatch.Start();
-            var x = yieldNToList.GetYieldSlow(10).ToList();
-            //var x = yieldNToList.GetYieldSlow(10);
+            var x = yieldNToList.GetYieldSlow(10).ToList(); // com o to list, iremos alocar a lista inteira na memória, o que pode ser ruim para grandes quantidades de dados
+            //var x = yieldNToList.GetYieldSlow(10); // sem o to list, iremos iterar sobre os elementos todas as vezes
+            //                                          em que Sum() o IEnumerable for usado, o que pode ser mais lento mas não irá alocar a lista inteira na memória
             Console.WriteLine(x.Sum());
             Console.WriteLine(x.Sum());
             stopWatch.Stop();
