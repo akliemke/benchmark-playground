14310083e1de5322dffc1e6c94ae28dc7b995373
Albert Kliemke <akliemke@protonmail.com>
2024-06-29 22:45:43 -0300

    testing lockslim

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using lock_vs_readerwriteslimlock;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lockSlim = new ReaderWriterLockSlimDemo();
+            lockSlim.TaskRunner();
+        }
+
+    }
+}
---
+++ lock-vs-readerwriteslimlock/LockBench.cs
@@ -0,0 +1,83 @@
+﻿namespace lock_vs_readerwriteslimlock
+{
+    public class ReaderWriterLockSlimDemo
+    {
+        public static ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        private Dictionary<string, string> cache = new Dictionary<string, string>();
+
+        public Dictionary<string, string> Cache
+        {
+            get
+            {
+                try
+                {
+                    _lock.EnterReadLock();
+                    return cache;
+                }
+                finally
+                {
+                    _lock.ExitReadLock();
+                }
+            }
+        }
+
+        public ReaderWriterLockSlimDemo()
+        {
+            cache.Add("key1", "1");
+            cache.Add("key2", "2");
+            cache.Add("key3", "3");
+            cache.Add("key4", "4");
+            cache.Add("key5", "5");
+        }
+
+        public void RenewCache()
+        {
+            try
+            {
+                _lock.EnterWriteLock();
+                cache = new Dictionary<string, string>();
+                cache.Add("key1", "1");
+                cache.Add("key2", "2");
+                cache.Add("key3", "3");
+                cache.Add("key4", "4");
+                cache.Add("key5", "5");
+                Thread.Sleep(10);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+
+        }
+
+        public async Task TaskRunner()
+        {
+
+
+            Parallel.ForEach(Enumerable.Range(0, 10), (item) =>
+            {
+                Console.WriteLine($"Creating Task {item}");
+                while (true)
+                {
+                    foreach (var index in Enumerable.Range(0, 6))
+                    {
+                        Cache.TryGetValue($"key{index}", out var val);
+                        Console.WriteLine($"task:{item} valor: {val}");
+                        //Thread.Sleep(500);
+
+                        if (index == 5)
+                        {
+                            RenewCache();
+                        }
+                    }
+                }
+
+
+                Console.WriteLine(item);
+
+            });
+            Console.WriteLine("Press any key to exit");
+
+        }
+    }
+}
